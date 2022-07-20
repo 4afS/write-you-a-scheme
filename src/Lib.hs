e89fe6693d5260e38967c5004491d8f2121c24ab
4afS <4afS.h1@gmail.com>
2022-07-21 08:59:58 +0900

    WIP: 2022-07-21

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Lib where
+
+import Data.Text as T
+import qualified Data.Map as Map
+import Control.Monad.Reader
+import Control.Monad.Except
+import LispVal
 
 run :: IO ()
 run = putStrLn "main running..."
