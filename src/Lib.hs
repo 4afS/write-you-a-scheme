7b6bf4a58e5293d6944259233c7e82e59d1cb8d2
4afS <4afS.h1@gmail.com>
2022-07-23 15:44:42 +0900

    exercises 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,21 +1,28 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Lib where
 
 import           Control.Monad.Except
 import           Control.Monad.Reader
 import qualified Data.Map                      as Map
-import           Data.Text                     as T
+import qualified Data.Text                     as T
 
+import           Data.Char                     (digitToInt)
+import           Numeric
+import           Text.Parsec                   (Parsec)
 import           Text.ParserCombinators.Parsec hiding (spaces)
+
 data LispVal
   = Atom String
   | List [LispVal]
   | DottedList [LispVal] LispVal
   | Number Integer
+  | Float Float
   | String String
+  | Char Char
   | Bool Bool
+  deriving (Eq, Show)
 
 run :: IO ()
 run = putStrLn "main running..."
@@ -29,9 +36,20 @@ spaces = skipMany1 space
 parseString :: Parser LispVal
 parseString = do
   char '"'
-  x <- many (noneOf "\"")
+  x <- many (noneOf "\"" <|> escapeChar)
   char '"'
   return $ String x
+
+escapeChar :: Parser Char
+escapeChar = do
+  char '\\'
+  e <- oneOf "\"nt\\"
+  return $
+    case e of
+      '"'  -> '\"'
+      '\\' -> '\\'
+      'n'  -> '\n'
+      't'  -> '\t'
 
 parseAtom :: Parser LispVal
 parseAtom = do
@@ -42,8 +60,60 @@ parseAtom = do
     "#f" -> Bool False
     atom -> Atom atom
 
+-- parseChar :: Parser LispVal
+-- parseChar = do
+--   string "#\\"
+--   c <- (specialized <$> string "") <|> letter <|> digit <|> symbol <|> (specialized <$> (string "space" <|> string "newline"))
+--   return . Char $ c
+--   where
+--     specialized "space"  = ' '
+--     specialized ""        = ' '
+--     specialized "newline" = '\n'
+
+parseChar :: Parser LispVal
+parseChar = do
+  string "#\\"
+  c <- specializedCharString "space" <|> specializedCharString "newline" <|> letter <|> digit <|> symbol <|> specializedCharString ""
+  return . Char $ c
+  where
+    charString :: String -> Char
+    charString s =
+      case s of
+        "newline" -> '\n'
+        "space"   -> ' '
+        -- TODO: Maybe should fix this
+        ""        -> ' '
+    specializedCharString :: String -> Parsec String () Char
+    specializedCharString s = do
+      ss <- string s
+      return . charString $ ss
+
 parseNumber :: Parser LispVal
-parseNumber = Number . read <$> many1 digit
+parseNumber = do
+  (Number . read <$> many1 digit) <|> parsePrefixedNumber
+
+parsePrefixedNumber :: Parser LispVal
+parsePrefixedNumber = do
+  char '#'
+  r <- oneOf "bodx"
+  ((n, _) : _) <- case r of
+    'b' -> readBin <$> many1 (oneOf "01")
+    'o' -> readOct <$> many1 (oneOf "01234567")
+    'd' -> readDec <$> many1 digit
+    'x' -> readHex <$> many1 (digit <|> oneOf "abcdef")
+  return $ Number n
+
+readBin :: String -> [(Integer, String)]
+readBin s = return ((fromIntegral . T.foldl' (\acc x -> acc * 2 + digitToInt x) 0 . T.pack)  s, "")
+
+parseFloat :: Parser LispVal
+parseFloat = do
+  n <- many1 digit
+  dot <- char '.'
+  m <- many1 digit
+  let ((f, _): _) = readFloat $ n ++ dot : m
+  return . Float $ f
+
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom <|> parseString <|> parseNumber
@@ -53,9 +123,13 @@ parseExpr = parseAtom <|> parseString <|> parseNumber
 -- WAS WAS NOW "No match found: \"lisp\" (line 1, column 1):\nunexpected \"a\""
 -- WAS NOW "No match found: \"lisp\" (line 1, column 1):\nunexpected \" \""
 -- NOW "Found: '%'"
+-- readExpr :: String -> String
+-- readExpr input = case parse (spaces >> symbol) "lisp" input of
+--   Left err -> "No match found: " ++ show err
+--   Right v  -> "Found: " ++ show v
+
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseString "lisp" input of
   Left err -> "No match found: " ++ show err
   Right v  -> "Found: " ++ show v
-
-
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,63 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+import           Data.Text                     (unpack)
+import qualified Lib                           as L
+import           NeatInterpolation
+import           Test.Hspec
+import           Text.ParserCombinators.Parsec hiding (spaces)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Lib.parseString" $ do
+    it "数字と文字だけで構成された文字列をパースできる" $ do
+      let s = unpack [text| "abc123" |]
+      parse L.parseString "test" s `shouldBe` Right (L.String "abc123")
+    it "バックスラッシュを含む文字列をパースできる" $ do
+      let s = unpack [text| "abc\123" |]
+      parse L.parseString "test" s `shouldBe` Right (L.String "abc\\123")
+    it "改行を含む文字列をパースできる" $ do
+      let s = "\"abc\n123\""
+      parse L.parseString "test" s `shouldBe` Right (L.String "abc\n123")
+    it "タブを含む文字列をパースできる" $ do
+      let s = "\"abc\t123\""
+      parse L.parseString "test" s `shouldBe` Right (L.String "abc\t123")
+  describe "Lib.parseNumber" $ do
+    it "数字だけで構成された文字列をパースできる" $ do
+      let s = "123"
+      parse L.parseNumber "test" s `shouldBe` Right (L.Number 123)
+    it "#bから始まる2進数の文字列をパースできる" $ do
+      let s = "#b111"
+      parse L.parseNumber "test" s `shouldBe` Right (L.Number 7)
+    it "#oから始まる8進数の文字列をパースできる" $ do
+      let s = "#o111"
+      parse L.parseNumber "test" s `shouldBe` Right (L.Number 73)
+    it "#dから始まる10進数の文字列をパースできる" $ do
+      let s = "#d111"
+      parse L.parseNumber "test" s `shouldBe` Right (L.Number 111)
+    it "#dから始まる10進数の文字列をパースできる" $ do
+      let s = "#x111"
+      parse L.parseNumber "test" s `shouldBe` Right (L.Number 273)
+  describe "Lib.parseChar" $ do
+    it "数字の文字をパースできる" $ do
+      let s = unpack [text| #\1 |]
+      parse L.parseChar "test" s `shouldBe` Right (L.Char '1')
+    it "アルファベットの文字をパースできる" $ do
+      let s = unpack [text| #\a |]
+      parse L.parseChar "test" s `shouldBe` Right (L.Char 'a')
+    it "記号の文字をパースできる" $ do
+      let s = unpack [text| #\% |]
+      parse L.parseChar "test" s `shouldBe` Right (L.Char '%')
+    it "newlineの文字をパースできる" $ do
+      let s = unpack [text| #\newline |]
+      parse L.parseChar "test" s `shouldBe` Right (L.Char '\n')
+    it "spaceの文字をパースできる" $ do
+      let s = unpack [text| #\space |]
+      parse L.parseChar "test" s `shouldBe` Right (L.Char ' ')
+    it "何も無い場合にspaceの文字としてパースできる" $ do
+      let s = unpack [text| #\ |]
+      parse L.parseChar "test" s `shouldBe` Right (L.Char ' ')
+  describe "Lib.parseFloat" $ do
+    it "小数点を含む数字列をパースできる" $ do
+      let s = unpack [text| 123.456 |]
+      parse L.parseFloat "test" s `shouldBe` Right (L.Float 123.456)
+
